2d9f76075ecb24e3fe8f038237a7be0da0d29629
Carlos Alejandro La Hoz <78973956+xCarlosDevx@users.noreply.github.com>
2023-12-04 01:46:03 -0400

    fix in drawer and router

--- app/(drawer)/home/index.jsx
+++ app/(drawer)/home/index.jsx
@@ -1,15 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 import React from "react";
-export default function Home() {
+export default function HomePage() {
   return (
     <View style={styles.container}>
       <View style={styles.imgContainer}>
-        <Image style={styles.image} source={require("../../assets/logo.jpg")} />
+        <Image
+          style={styles.image}
+          source={require("../../../assets/logo.png")}
+        />
       </View>
       <Text style={styles.text}>DigeAgent</Text>
       {/* <Button title="Tools" onPress={() => navigation.openDrawer()} /> */}
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -31,14 +32,13 @@ const styles = StyleSheet.create({
   image: {
     borderWidth: 2,
     borderColor: "#282828",
-    borderRadius: "50%",
+    borderRadius: 100,
     resizeMode: "contain",
-    height: "100%",
-    width: "100%",
+    height: 200,
+    width: 200,
   },
   text: {
     fontSize: 20,
-    fontWeight: 600,
     margin: 0,
     marginBottom: 20,
   },
