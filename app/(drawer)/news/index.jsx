15c291483ceb3e9ecdb7378e349f1864732d4569
Juan Wody Jiménez Soto <20198497@itla.edu.do>
2023-12-11 13:31:32 -0400

    Updating news page again

--- app/(drawer)/news/index.jsx
+++ app/(drawer)/news/index.jsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View} from "react-native";
 import { WebView } from 'react-native-webview';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getNoticias } from "../../../api/noticiasAPI"
+
 export default function NewsPage() {
 
+  
   return (
-    <View>
-    </View>
-
+    <WebView
+      source={{ uri: 'https://remolacha.net/#gsc.tab=0' }}
+      style={{ marginTop: 20 }}
+    />
   );
 }
